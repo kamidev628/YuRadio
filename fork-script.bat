cd05e66315ee213ec791fd4184093c835f09bdb9
kamidev628 <marcin.nowak.0628@proton.me>
2025-03-06 11:59:11 -0500

    Clean repo

--- fork-script.bat
+++
@@ -1,5 +0,0 @@
-git remote set-url origin %1
- 
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='kamidev628'; GIT_AUTHOR_EMAIL='marcin.nowak.0628@proton.me'; GIT_COMMITTER_NAME='kamidev628'; GIT_COMMITTER_EMAIL='marcin.nowak.0628@proton.me';"
- 
-git push --force origin master
